45b354f7938777187f5f7cdfe112d73b4e185682
Mateusz <mateusz.rudnicki05@wp.pl>
2026-07-10 18:00:13 +0200

    Add Gym model

--- gym/models.py
+++ gym/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Gym(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=128)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+
+
+
