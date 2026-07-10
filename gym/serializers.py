0a68e787d2846dc4e3330402bccb9b5e4e9f7bf5
Mateusz <mateusz.rudnicki05@wp.pl>
2026-07-10 22:14:08 +0200

    New Serializers

--- gym/serializers.py
+++ gym/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
 
-from gym.models import Gym, Studio, Trainer
+from gym.models import Gym, Studio, Trainer, User, Discipline, TrainingSession, Reservation
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password", "email", "phone_number", "date_of_birth", "membership_level"]
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
 
 
 class GymSerializer(serializers.ModelSerializer):
@@ -18,4 +29,22 @@ class StudioSerializer(serializers.ModelSerializer):
 class TrainerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trainer
-        fields = ["id", "name", "gym", "experience_years"]
+        fields = ["id", "first_name", "last_name", "experience_years", "description"]
+
+
+class DisciplineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discipline
+        fields = ["id", "name", "description"]
+
+
+class TrainingSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainingSession
+        fields = ["id", "trainer", "discipline", "studio", "start_time", "end_time"]
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ["id", "user", "training_session"]
