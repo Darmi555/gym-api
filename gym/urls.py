698f625c22bd2657dac21cf04798e4a6a26d7d10
Mateusz <mateusz.rudnicki05@wp.pl>
2026-07-10 20:56:34 +0200

    Add Trainder model, serializer, view and router

--- gym/urls.py
+++ gym/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from gym.views import GymViewSet, StudioViewSet
+from gym.views import GymViewSet, StudioViewSet, TrainerViewSet
 
 router = DefaultRouter()
 router.register("gyms", GymViewSet)
 router.register("studios", StudioViewSet)
+router.register("trainers", TrainerViewSet)
 
 urlpatterns = router.urls
