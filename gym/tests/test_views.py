5fd340956b6d9249f1b357e96f956dda2ec6fe7b
Darmi555 <mateusz.rudnicki05@wp.pl>
2026-07-16 14:00:18 +0200

    Add API test for user registration

--- gym/tests/test_views.py
+++ gym/tests/test_views.py
@@ -1,6 +1,27 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class UserRegistrationApiTest(APITestCase):
+    def test_registration_creates_user_and_hides_password(self):
+        response = self.client.post(
+            "/api/gym/users/",
+            data={
+                "email": "test@test.com",
+                "password": "testpassowrd",
+                "username": "testuser",
+            }
+        )
+        user = User.objects.get(email="test@test.com")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(user.email, "test@test.com")
+        self.assertTrue(user.check_password("testpassowrd"))
+        self.assertNotIn("password", response.data)
+
 
 class UnauthenticatedReservationApiTest(APITestCase):
     def test_auth_required(self):
